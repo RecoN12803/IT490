0b0adb0bb52d1c3970c57261ea3eac74130ce0a5
RecoN12803 <131042605+RecoN12803@users.noreply.github.com>
2025-06-09 13:56:58 -0400

    making setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+//This is my setup script jh699
+apt update -y
+apt upgrade -y 
+apt install -y \ 
+  net-tools \
+  unzip \ 
+  ssh \ 
+  nano \
+
+wget https"//github.com/MattToegel/IT490/archive/refs/heads/main.zip
