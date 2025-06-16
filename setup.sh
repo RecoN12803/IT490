bb8403a40b73fb69556fa841275bace97f64aec3
RecoN12803 <131042605+RecoN12803@users.noreply.github.com>
2025-06-15 23:18:25 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 //This is my setup script jh699
 sudo apt update -y
 sudo apt upgrade -y 
@@ -6,5 +8,16 @@ sudo apt install -y \
   unzip \ 
   ssh \ 
   nano \
+  rabbitmq-server \
+  git\ 
+
+sudo systemctl enable rabbitmq-server
+sudo systemctl start rabbitmq-server
+
+sudo rabbitmqctl add_user it490 it490pass 
+sudo rabbitmqctl set_user_tags it490 administrator
+sudo rabbitmqctl set_permissions -p / it490 ".*" ".*" ".*"
+
+curl -fsSL https://tailscale.com/install.sh | sh
 
 wget https"//github.com/MattToegel/IT490/archive/refs/heads/main.zip
